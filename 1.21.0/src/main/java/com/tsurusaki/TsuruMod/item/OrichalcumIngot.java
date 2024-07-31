7858cb671acee7806e68d811520413c958e72267
ryo-tsurusaki <tsurusaki.ryo@gmail.com>
2024-07-31 19:15:35 +0900

    ログを仕込んでみる

--- 1.21.0/src/main/java/com/tsurusaki/TsuruMod/item/OrichalcumIngot.java
+++ 1.21.0/src/main/java/com/tsurusaki/TsuruMod/item/OrichalcumIngot.java
@@ -2,6 +2,7 @@ package com.tsurusaki.TsuruMod.item;
 
 import com.tsurusaki.TsuruMod.constants.item.StacksTo;
 import com.tsurusaki.TsuruMod.registry.ModTags;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -11,8 +12,12 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class OrichalcumIngot extends Item {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrichalcumIngot.class);
 
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(
@@ -20,8 +25,10 @@ public class OrichalcumIngot extends Item {
             Player player,
             @NotNull InteractionHand hand) {
 
-        ItemStack itemStack = player.getItemInHand(hand);
+        player.sendSystemMessage(Component.literal("OrichalcumIngot used."));
+        LOGGER.debug("OrichalcumIngot used.");
 
+        ItemStack itemStack = player.getItemInHand(hand);
         if (itemStack.is(ModTags.Items.IS_SHINE)) {
             // 3秒間光らせる
             player.addEffect(new MobEffectInstance(MobEffects.GLOWING, 20 * 3, 1));
