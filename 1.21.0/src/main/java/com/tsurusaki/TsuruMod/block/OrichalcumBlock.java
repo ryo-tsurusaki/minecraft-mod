66e3112dd34e8d7cdac5438041c36d36ec968adb
ryo-tsurusaki <tsurusaki.ryo@gmail.com>
2024-08-06 19:03:49 +0900

    固くする

--- 1.21.0/src/main/java/com/tsurusaki/TsuruMod/block/OrichalcumBlock.java
+++ 1.21.0/src/main/java/com/tsurusaki/TsuruMod/block/OrichalcumBlock.java
@@ -6,8 +6,8 @@ import net.minecraft.world.level.block.SoundType;
 public class OrichalcumBlock extends Block {
     public OrichalcumBlock() {
         super(Properties.of()
-                .strength(5.0F, 1500F)
-                .sound(SoundType.ANVIL)
+                .strength(1.0F, 1500F)
+                .sound(SoundType.AMETHYST_CLUSTER)
                 .lightLevel((light) -> 15)
                 .requiresCorrectToolForDrops()
         );
